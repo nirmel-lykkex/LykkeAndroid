e0e0dc14b1fd79b54a9a14ecfb7b5186f2ed2a60
e.kazimirova <e.kazimirova@itransition.com>
2016-02-12 23:34:37 +0300

    ANDROIWD-4

--- LykkeWallet/app/src/main/java/com/lykkex/LykkeWallet/gui/SelfieActivity.java
+++ LykkeWallet/app/src/main/java/com/lykkex/LykkeWallet/gui/SelfieActivity.java
@@ -1,14 +1,120 @@
 package com.lykkex.LykkeWallet.gui;
 
 import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.lykkex.LykkeWallet.R;
+import com.lykkex.LykkeWallet.gui.fragments.CameraPreview;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by e.kazimirova on 12.02.2016.
  */
 @EActivity(R.layout.selfie_activity)
-public class SelfieActivity extends Activity{
+public class SelfieActivity extends Activity {
+    private android.hardware.Camera mCamera;
+    private CameraPreview mCameraPreview;
+
+    /** Called when the activity is first created. */
+    @AfterViews
+    public void afterViews() {
+        mCamera = getCameraInstance();
+        mCameraPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mCameraPreview);
+
+        Button captureButton = (Button) findViewById(R.id.buttake_photo);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.takePicture(null, null, mPicture);
+            }
+        });
+    }
+
+    /**
+     * Helper method to access the camera returns null if it cannot get the
+     * camera or does not exist
+     *
+     * @return
+     */
+    private android.hardware.Camera getCameraInstance() {
+        android.hardware.Camera camera = null;
+        try {
+            camera = android.hardware.Camera.open();
+        } catch (Exception e) {
+            // cannot get camera or does not exist
+        }
+        return camera;
+    }
+
+    android.hardware.Camera.PictureCallback mPicture = new android.hardware.Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, android.hardware.Camera camera) {
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null) {
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+
+            } catch (IOException e) {
+            }
+        }
+    };
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+                .format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
+    }
+
+    private Camera openFrontFacingCameraGingerbread() {
+        int cameraCount = 0;
+        Camera cam = null;
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        cameraCount = Camera.getNumberOfCameras();
+        for (int camIdx = 0; camIdx < cameraCount; camIdx++) {
+            Camera.getCameraInfo(camIdx, cameraInfo);
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                try {
+                    cam = Camera.open(camIdx);
+                } catch (RuntimeException e) {
+
+                }
+            }
+        }
+
+        return cam;
+    }
 }
